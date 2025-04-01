d3cee15ed6ad872551bc0492286a6e85c8d0251d
Derck23 <eramireznieves25@gmail.com>
2025-03-31 20:03:19 -0600

    produccion casi publicacion

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/api",
+  /*baseURL: "http://localhost:3001/api",*/
+  baseURL: "https://dwp-backend-carcoleccion.up.railway.app/api",
   headers: {
     "Content-Type": "application/json",
   },
